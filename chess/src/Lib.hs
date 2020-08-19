9a1a73ac55198f471f9be03dd8ea98bdc01279da
Toby <tobyjonahbee@gmail.com>
2020-08-19 12:22:59 +0100

    Successful start on checking update validity - the board cannot stay the same!

--- chess/src/Lib.hs
+++ chess/src/Lib.hs
@@ -14,12 +14,12 @@ data Vec (n :: Nat) (a :: Type) where
 infixr 4 :->
 
 -- Helper type family, to avoid the (:-> VEnd) bit.
-type family (:<>:) (x :: a) (y :: a) :: Vec 2 a where
-    x :<>: y = x :-> (y :-> VEnd)
-infixr 5 :<>:
+type family (:<>) (x :: a) (y :: a) :: Vec 2 a where
+    x :<> y = x :-> (y :-> VEnd)
+infixr 5 :<>
 
 -- Type synonym for an 8x8 grid
-type Grid8x8 = Vec 8 (Vec 8 Piece)
+type Grid8x8 = Vec 8 (Vec 8 (Maybe Piece))
 
 -- TODO: Dimensions of board in type??
 data Board where
@@ -44,10 +44,11 @@ data PieceName = Pawn
                | King
                | Queen
 
--- Holds the number of moves they've made.
--- (Their current position is implicit from where they are in the board!)
+-- Holds the number of moves they've made, plus their current position.
+-- While their position is implicit from where they are in the board, it's
+-- helpful!
 data PieceInfo where
-    Info :: Nat -> PieceInfo
+    Info :: Nat -> Position -> PieceInfo
 
 -- TODO: Type level char??
 -- TODO: Make operator for adding a Nat to a row name, creating a new row name (e.g. "a" :+: 1)
@@ -97,37 +98,57 @@ data Proxy a = Proxy
 -- TEST TYPES
 -- TODO: Remove these
 type TestPosition = At "a" 1
-type TestPiece    = MkPiece Black Pawn (Info 0)
-type TestColumn   = TestPiece :-> (TestPiece :-> (TestPiece :-> (TestPiece :-> (TestPiece :-> (TestPiece :-> (TestPiece :-> (TestPiece :-> VEnd)))))))
-type TestBoard    = TestColumn :-> (TestColumn :-> (TestColumn :-> (TestColumn :-> (TestColumn :-> (TestColumn :-> (TestColumn :-> (TestColumn :-> VEnd)))))))
+type TestPiece    = MkPiece Black Pawn (Info 0 TestPosition)
+type EmptyRow     = Nothing :-> Nothing :-> Nothing :-> Nothing :-> Nothing :-> Nothing :-> Nothing :<> Nothing
+type TestBoard    = (Just TestPiece :-> Nothing :-> Nothing :-> Nothing :-> Nothing :-> Nothing :-> Nothing :<> Nothing)
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :<> EmptyRow
+type TestBoard2   = (Just TestPiece :-> Nothing :-> Nothing :-> Nothing :-> Nothing :-> Nothing :-> Nothing :<> Nothing)
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :-> EmptyRow
+                    :<> EmptyRow
 
 type family Update (board :: Board) (pieces :: Vec n Piece) (positions :: Vec n Position) :: Board where
     Update board pieces positions = TypeError (Text "Unfinished!")
 
-type family IsUpdateValid (from :: Board) (to :: Board) :: Board where
-    IsUpdateValid _ _ = TypeError (Text "Unfinished!")
+type family IsUpdateValid (from :: Board) (to :: Board) (turn :: Team) :: Board where
+    IsUpdateValid x x _ = TypeError (Text "A move must be made - the board cannot stay exactly the same.")
+    IsUpdateValid _ _ _ = TypeError (Text "Unfinished!")
 
--- TODO: Make this work
--- Associated type family for pieces that can move; takes their current position, the board,
--- and then a series of valid positions they can move to.
--- TODO: Use type equality (~) to check that the piece is at that position
-class Moveable p where
-    type NextPositions p :: Position -> Board -> Exp (Vec n Position)
+-- Rudimentary way to display type errors, for now.
+x :: Proxy (IsUpdateValid (MkBoard TestBoard) (MkBoard TestBoard2) White)
+x = Proxy
 
--- Defunctionalised evaluation of moveable pieces
-type Exp a = a -> Type
+-- -- TODO: Make this work
+-- -- Associated type family for pieces that can move; takes their current position, the board,
+-- -- and then a series of valid next boards!
+-- -- TODO: Use type equality (~) to check that the piece is at that position
+-- class Moveable p where
+--     type NextPositions p :: Position -> Board -> Exp (Vec n Position)
 
-type family Eval (e :: Exp a) :: a
+-- -- Defunctionalised evaluation of moveable pieces
+-- type Exp a = a -> Type
 
-data MovePawn :: Position -> Board -> Exp (Vec n Position)
+-- type family Eval (e :: Exp a) :: a
 
--- TODO: Complete the below by checking the piece at the location!
--- FIXME: Pawns move in different directions depending on their color - fix this
--- FIXME: Pawns move 1 or 2 spaces depending on whether it's their first move or not
-type instance Eval (MovePawn (At row col) b) = (At row (col + 1)) :-> ((At row (col + 2)) :-> VEnd)
+-- data MovePawn :: Position -> Board -> Exp (Vec n Position)
 
-instance Moveable Pawn where
-    type NextPositions Pawn = MovePawn
+-- -- TODO: Complete the below by checking the piece at the location!
+-- -- FIXME: Pawns move in different directions depending on their color - fix this
+-- -- FIXME: Pawns move 1 or 2 spaces depending on whether it's their first move or not
+-- type instance Eval (MovePawn (At row col) b) = (At row (col + 1)) :-> ((At row (col + 2)) :-> VEnd)
+
+-- instance Moveable Pawn where
+--     type NextPositions Pawn = MovePawn
 
 -- type family MovePawn (pos :: Position) (b :: Board) :: Vec n Position where
 --     MovePawn x y = TestPosition :-> VEnd
