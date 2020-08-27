82f4afa57d9455f540d2ed66b8a116bfd7cfcfe9
Toby <tobyjonahbee@gmail.com>
2020-08-27 16:21:32 +0100

    Removing unused stuffs!

--- chess/src/Lib.hs
+++ chess/src/Lib.hs
@@ -197,13 +197,6 @@ data PieceInfo where
 data Position where
     At :: Symbol -> Nat -> Position
 
-type Move = (Piece, Position)
-
--- TODO: Constrain so n < 3??
--- Can get the piece from the initial Position pair
-data Moves where
-    Moves :: Vec n Move -> Moves
-
 type ValidColumns = "a" :-> "b" :-> "c" :-> "d" :-> "e" :-> "f" :-> "g" :<> "h"
 
 type family ValidColumn (row :: Symbol) :: Maybe Symbol where
@@ -214,11 +207,12 @@ data MyNat where
     Z :: MyNat
     S :: MyNat -> MyNat
 
-data NatToMyNat :: Nat -> Exp (MyNat)
-type instance Eval (NatToMyNat n) = NatToMyNatNonFCF n
-data MyNatToNat :: MyNat -> Exp (Nat)
+data MyNatToNat :: MyNat -> Exp Nat
 type instance Eval (MyNatToNat Z)     = 0
 type instance Eval (MyNatToNat (S n)) = 1 + (Eval (MyNatToNat n))
+
+data NatToMyNat :: Nat -> Exp MyNat
+type instance Eval (NatToMyNat n) = NatToMyNatNonFCF n
 
 type family NatToMyNatNonFCF (n :: Nat) :: MyNat where
     NatToMyNatNonFCF 0 = Z
@@ -273,20 +267,12 @@ type TestBoard2   = (Just TestPiece :-> Nothing :-> Nothing :-> Nothing :-> Noth
                     :-> EmptyRow
                     :<> EmptyRow
 
-type family UpdateBoard (board :: Board) (turn :: Team) (moves :: Moves) :: Board where
-    UpdateBoard _ _ ('Moves VEnd) = TypeError (Text "A move must be made!")
-    UpdateBoard _ _ _    = TypeError (Text "UpdateBoard is unfinished!")
-
-type family IsUpdateValid (from :: Board) (to :: Board) (turn :: Team) :: Board where
-    IsUpdateValid x x _ = TypeError (Text "A move must be made - the board cannot stay exactly the same.")
-    IsUpdateValid _ _ _ = TypeError (Text "IsUpdateValid is unfinished!")
-
 -- When using Maybes, this returns another maybe!
 -- :kind! Eval (VecAt TestBoard Z) :: Maybe (Vec 8 (Maybe Piece))
 -- data Bind :: (a -> Exp (f b)) -> f a -> Exp (f b)
 data VecAt :: Vec n a -> MyNat -> Exp (Maybe a)
-type instance Eval (VecAt VEnd _) = Nothing
-type instance Eval (VecAt (x :-> xs) Z) = Just x
+type instance Eval (VecAt VEnd _)           = Nothing
+type instance Eval (VecAt (x :-> xs) Z)     = Just x
 type instance Eval (VecAt (x :-> xs) (S n)) = Eval (VecAt xs n)
 
 data (!!) :: Vec n a -> MyNat -> Exp (Maybe a)
@@ -305,10 +291,6 @@ type family ColToIndex (col :: Symbol) :: Maybe Nat where
 data GetPieceAt :: Board -> Position -> Exp (Maybe Piece)
 type instance Eval (GetPieceAt board (At col row)) = Eval (Join (Eval (Join (Eval ((Eval ((CW (!!)) <$> (Eval (board !! (Eval (NatToMyNat (row - 1))))))) <*> (Eval (NatToMyNat <$> (ColToIndex col))))))))
 
--- Rudimentary way to display type errors, for now.
-x :: Proxy (UpdateBoard TestBoard White ('Moves VEnd))
-x = Proxy
-
 data IsBlack :: Piece -> Exp (Bool)
 type instance Eval (IsBlack (MkPiece team _ _)) = Eval (team :==: Black)
 
@@ -322,6 +304,7 @@ data CalculateValidMoves :: Position -> Board -> Exp (Vec n Position)
 type instance Eval (CalculateValidMoves pos board) = Eval (FromMaybe VEnd ((Flip PieceCanMoveTo) board) (Eval (GetPieceAt board pos)))
 
 -- TODO: Write instances for each team x piece, e.g. White Pawn, Black Knight, ...
+-- TODO: Check that the piece's reported position is its' actual position
 data PieceCanMoveTo :: Piece -> Board -> Exp (Vec n Position)
 type instance Eval (PieceCanMoveTo (MkPiece Black Pawn info) board)   = TypeError (Text "Not written PieceCanMoveTo yet!")
 type instance Eval (PieceCanMoveTo (MkPiece White Pawn info) board)   = TypeError (Text "Not written PieceCanMoveTo yet!")
@@ -336,32 +319,6 @@ type instance Eval (PieceCanMoveTo (MkPiece White Queen info) board)  = TypeErro
 type instance Eval (PieceCanMoveTo (MkPiece Black King info) board)   = TypeError (Text "Not written PieceCanMoveTo yet!")
 type instance Eval (PieceCanMoveTo (MkPiece White King info) board)   = TypeError (Text "Not written PieceCanMoveTo yet!")
 
--- -- TODO: Check the piece's reported position is the actual position, eh
--- type family PieceMoves (p :: Piece) (board :: Board) :: Maybe (Vec n Position) where
---     PieceMoves (MkPiece team Pawn info) board = PawnMoves (MkPiece team Pawn info) board
---     PieceMoves _ _ = TypeError (Text "Unfinished!")
-
--- -- TODO: Include diagonal takes!
--- -- TODO: Take the board into account!
--- -- TODO: Return Nothing if no valid moves
--- type family PawnMoves (p :: Piece) (board :: Board) :: Maybe (Vec n Position) where
---     PawnMoves (MkPiece Black Pawn (Info 0 (At col row))) board = Just (At col (row - 1) :<> At col (row - 2))
---     PawnMoves (MkPiece Black Pawn (Info n (At col row))) board = Just (At col (row - 1) :-> VEnd)
---     PawnMoves (MkPiece White Pawn (Info 0 (At col row))) board = Just (At col (row + 1) :<> At col (row + 2))
---     PawnMoves (MkPiece White Pawn (Info n (At col row))) board = Just (At col (row + 1) :-> VEnd)
-
--- pawnMovesTest1 :: Proxy (Just (At "a" 3 :<> At "a" 2))
--- pawnMovesTest1 = Proxy @(PawnMoves (MkPiece Black Pawn (Info 0 (At "a" 4))) TestBoard)
-
--- pawnMovesTest2 :: Proxy (Just (At "a" 3 :-> VEnd))
--- pawnMovesTest2 = Proxy @(PawnMoves (MkPiece Black Pawn (Info 7 (At "a" 4))) TestBoard)
-
--- pawnMovesTest3 :: Proxy (Just (At "a" 5 :<> At "a" 6))
--- pawnMovesTest3 = Proxy @(PawnMoves (MkPiece White Pawn (Info 0 (At "a" 4))) TestBoard)
-
--- pawnMovesTest4 :: Proxy (Just (At "a" 5 :-> VEnd))
--- pawnMovesTest4 = Proxy @(PawnMoves (MkPiece White Pawn (Info 7 (At "a" 4))) TestBoard)
-
 getPieceAtTest1 :: Proxy (Just TestPiece)
 getPieceAtTest1 = Proxy @(Eval (GetPieceAt TestBoard (At "a" 1)))
 
@@ -369,7 +326,6 @@ getPieceAtTest1 = Proxy @(Eval (GetPieceAt TestBoard (At "a" 1)))
 getPieceAtTest2 :: Proxy (Just TestPiece)
 getPieceAtTest2 = Proxy @(Eval (Join (Eval (Bind ((Flip (!!)) (Eval (NatToMyNat 0))) (Eval (TestBoard !! (Eval (NatToMyNat 0))))))))
 
--- TODO: Fix this!! Use bind!
 -- :kind! VecAt (Z :<> (S Z)) :: MyNat -> Exp (Maybe MyNat)
 getPieceAtTest3 :: Proxy (Just Z)
 getPieceAtTest3 = Proxy @(Eval (Join (Eval ((Eval ((CW (!!)) <$> Just (Z :<> (S Z)))) <*> Just Z))))
@@ -379,34 +335,3 @@ filterMapTest1 = Proxy @(Eval (FilterMap IsJust FromJust (Nothing :<> Nothing)))
 
 filterMapTest2 :: Proxy ("a" :-> "c" :-> "abcd" :-> VEnd)
 filterMapTest2 = Proxy @(Eval (FilterMap IsJust FromJust (Just "a" :-> Nothing :-> Just "c" :-> Just "abcd" :-> Nothing :-> Nothing :<> Nothing)))
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
--- type family Update (board :: Board) (pieces :: Vec n Piece) (positions :: Vec n Position) :: Board where
---     Update board pieces positions = TypeError (Text "Unfinished!")
-
--- -- TODO: Make this work
--- -- Associated type family for pieces that can move; takes their current position, the board,
--- -- and then a series of valid next positions!
--- -- TODO: Use type equality (~) to check that the piece is at that position
--- class Moveable p where
---     type NextPositions p :: Position -> Board -> Exp (Vec n Position)
-
--- -- Defunctionalised evaluation of moveable pieces
--- type Exp a = a -> Type
-
--- type family Eval (e :: Exp a) :: a
-
--- data MovePawn :: Position -> Board -> Exp (Vec n Position)
-
--- -- TODO: Complete the below by checking the piece at the location!
--- -- FIXME: Pawns move in different directions depending on their color - fix this
--- -- FIXME: Pawns move 1 or 2 spaces depending on whether it's their first move or not
--- type instance Eval (MovePawn (At row col) b) = (At row (col + 1)) :-> ((At row (col + 2)) :-> VEnd)
-
--- instance Moveable Pawn where
---     type NextPositions Pawn = MovePawn
-
--- type family MovePawn (pos :: Position) (b :: Board) :: Vec n Position where
---     MovePawn x y = TestPosition :-> VEnd
