a1c43e97e0f086359efc10593e60a73302271425
Toby <tobyjonahbee@gmail.com>
2020-10-18 18:10:42 +0100

    Using singletons for PieceName and Team! Not working for PieceInfo or Position though.

--- chess/src/TermToType.hs
+++ chess/src/TermToType.hs
@@ -3,27 +3,11 @@ module TermToType where
 import Data.Proxy(Proxy(..))
 import ChessTypes
 import GHC.TypeLits
+import Data.Singletons
+import Data.Singletons.TH
 
 -- Constraints for transforming term-level values to type-level, erm, types
 
--- Transform term-level piece name into type-level one
-class KnownPieceName (k :: PieceName) where
-    mkTypePiece :: PieceName -> Proxy k
-
-instance KnownPieceName 'Pawn where
-    mkTypePiece Pawn = Proxy @Pawn
-
-instance KnownPieceName 'Rook where
-    mkTypePiece Rook = Proxy @Rook
-
-instance KnownPieceName 'Bishop where
-    mkTypePiece Bishop = Proxy @Bishop
-
-instance KnownPieceName 'Knight where
-    mkTypePiece Knight = Proxy @Knight
-
-instance KnownPieceName 'King where
-    mkTypePiece King = Proxy @King
-
-instance KnownPieceName 'Queen where
-    mkTypePiece Queen = Proxy @Queen
+-- Use singletons where it doesn't throw horrible errors
+$(genSingletons [''PieceName])
+$(genSingletons [''Team])
