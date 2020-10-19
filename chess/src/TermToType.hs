3d22ac6fdc344990da650ffe06e5cb5fbe4e701e
Toby <tobyjonahbee@gmail.com>
2020-10-19 14:52:30 +0100

    Put together a little test file of Singletons for chess types!

--- chess/src/TermToType.hs
+++ chess/src/TermToType.hs
@@ -2,16 +2,19 @@ module TermToType where
 
 import Data.Proxy(Proxy(..))
 import ChessTypes
+import FirstClassFunctions
 import GHC.TypeLits
 import Data.Singletons
 import qualified Data.Singletons.TypeLits as TL
 import Data.Singletons.TH
+import Data.Singletons.Prelude.Bool
 
 -- Constraints for transforming term-level values to type-level, erm, types
 
 -- Use singletons where it doesn't throw horrible errors
 $(genSingletons [''PieceName])
 $(genSingletons [''Team])
+$(genSingletons [''MyNat])
 
 -- -- FIXME: Nasty errors, try and go through this
 -- -- 1. Could not deduce: Demote FirstClassFunctions.MyNat ~ FirstClassFunctions.MyNat
@@ -24,12 +27,20 @@ $(genSingletons [''Team])
 -- --     Actual type: Demote Symbol
 -- $(genSingletons [''Position])
 
+data MySymbol = A | B | C | D | E | F | G | H
+$(genSingletons [''MySymbol])
+
 data MyPosition where
-    At :: TL.Symbol -> TL.Nat -> MyPosition
+    At :: MySymbol -> MyNat -> MyPosition
 
 $(genSingletons [''MyPosition])
 
 data MyPieceInfo where
-    Info :: TL.Nat -> MyPosition -> Bool -> MyPieceInfo
+    Info :: MyNat -> MyPosition -> Bool -> MyPieceInfo
 
 $(genSingletons [''MyPieceInfo])
+
+data MyPiece where
+    MkPiece :: Team -> PieceName -> MyPieceInfo -> MyPiece
+
+$(genSingletons [''MyPiece])
