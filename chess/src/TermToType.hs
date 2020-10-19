61605e5f58cfea45dd5cc62d047886c1620bf57a
Toby <tobyjonahbee@gmail.com>
2020-10-19 16:25:11 +0100

    Now chess pieces are completely singleton types!

--- chess/src/TermToType.hs
+++ chess/src/TermToType.hs
@@ -10,26 +10,11 @@ import Data.Singletons.TH
 import Data.Singletons.Prelude.Bool
 import Data.Type.Nat hiding (SNat(..))
 
--- Constraints for transforming term-level values to type-level, erm, types
-
 -- Transform various types into singleton types (for use at the value-level)
 $(genSingletons [''PieceName])
 $(genSingletons [''Team])
 $(genSingletons [''Nat])
-
 $(genSingletons [''Column])
-
-data MyPosition where
-    At :: Column -> Nat -> MyPosition
-
-$(genSingletons [''MyPosition])
-
-data MyPieceInfo where
-    Info :: Nat -> MyPosition -> Bool -> MyPieceInfo
-
-$(genSingletons [''MyPieceInfo])
-
-data MyPiece where
-    MkPiece :: Team -> PieceName -> MyPieceInfo -> MyPiece
-
-$(genSingletons [''MyPiece])
+$(genSingletons [''Position])
+$(genSingletons [''PieceInfo])
+$(genSingletons [''Piece])
