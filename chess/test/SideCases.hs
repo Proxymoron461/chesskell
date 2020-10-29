558f262a55b4164a00f3fd4f86ae19e63ac1fa3e
Toby Bailey <tobyjonahbee@gmail.com>
2020-10-29 16:46:59 +0000

    SideCases now compiles (albeit horribly slowly). Maybe time for some optimisation?

--- chess/test/SideCases.hs
+++ chess/test/SideCases.hs
@@ -14,18 +14,18 @@ import TestTypes
 
 -- data IsSpaceVulnerableToEnPassant :: Team -> Board -> Position -> Exp Bool
 -- Team is the ATTACKING TEAM
-type WhitePawnFirst = FromJust' (Eval (Move (At A Nat2) (At A Nat4) StartBoard))
+type WhitePawnFirst = FromJust' (Eval (Move (At A Nat2) (At A Nat4) StartDec))
 enPassantTest1 :: True :~: Eval (IsSpaceVulnerableToEnPassant Black WhitePawnFirst (At A Nat4))
 enPassantTest1 = Refl
 
-enPassantTest2 :: False :~: Eval (IsSpaceVulnerableToEnPassant Black (FromJust' (Eval (Move (At A Nat2) (At A Nat3) StartBoard))) (At A Nat3))
+enPassantTest2 :: False :~: Eval (IsSpaceVulnerableToEnPassant Black (FromJust' (Eval (Move (At A Nat2) (At A Nat3) StartDec))) (At A Nat3))
 enPassantTest2 = Refl
 
-type BlackPawnFirst = FromJust' (Eval (Move (At A Nat7) (At A Nat5) StartBoard))
+type BlackPawnFirst = FromJust' (Eval (Move (At A Nat7) (At A Nat5) StartDec))
 enPassantTest3 :: True :~: Eval (IsSpaceVulnerableToEnPassant White BlackPawnFirst (At A Nat5))
 enPassantTest3 = Refl
 
-enPassantTest4 :: False :~: Eval (IsSpaceVulnerableToEnPassant White (FromJust' (Eval (Move (At A Nat7) (At A Nat6) StartBoard))) (At A Nat6))
+enPassantTest4 :: False :~: Eval (IsSpaceVulnerableToEnPassant White (FromJust' (Eval (Move (At A Nat7) (At A Nat6) StartDec))) (At A Nat6))
 enPassantTest4 = Refl
 
 enPassantTest5 :: False :~: Eval (IsSpaceVulnerableToEnPassant Black (FromJust' (Eval (Move (At A Nat7) (At A Nat6) WhitePawnFirst))) (At A Nat4))
