fcb00bfcca4f558306d0454ed05f90eb1bc26325
Toby Bailey <tobyjonahbee@gmail.com>
2020-12-01 19:57:23 +0000

    Implemented much of the README code from `sessiontypes`.

--- chess/src/ChessSessions.hs
+++ chess/src/ChessSessions.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RebindableSyntax #-}
+
 module ChessSessions where
 
 import Control.SessionTypes
@@ -8,11 +10,35 @@ import Data.Proxy
 import MakeSingletons
 import ChessTypes
 
-prog :: Monad m => STTerm m s s Team
-prog = return White
+prog :: Monad m => STTerm m ('Cap '[] (Int :?> Sel '[Bool :!> r, String :!> r])) ('Cap '[] r) ()
+prog = do
+  x <- recv
+  case x of
+    0 -> sel1 >> send True
+    n -> sel2 >> sel1 >> send "False"
 
-prog2 :: Monad m => STTerm m ('Cap '[] (Team :!> Team :?> Eps)) ('Cap '[] Eps) Team
-prog2 = send Black >> recv >>= eps
+progDual :: Monad m => STTerm m ('Cap '[] (Int :!> Off '[Int :?> r, String :?> r])) ('Cap '[] r) ()
+progDual = do
+  send 0
+  (recv >> return ()) <&> (recv >> return ())
 
-prog3 :: Monad m => STTerm m s s (Proxy StartDec)
-prog3 = return (Proxy @StartDec)
+sendChar :: Monad m => STTerm m ('Cap '[] ('R (Char :!> Off '[V, Wk Eps]))) ('Cap '[] Eps) ()
+sendChar = recurse go
+  where
+    go = do
+      send 'c'
+      offS (var0 >> go) (offZ $ (weaken0 >> eps0))
+
+prog_recursion :: MonadSession m => m ('Cap ctx (R (Int :?> Sel '[Wk Eps, V]))) ('Cap ctx Eps) Int
+prog_recursion = recurseFix $ \f -> do
+  x <- recv
+  if x < 10
+    then selN2 >> f
+    else sel1 >> weaken0 >> eps x
+
+prog_recursion_dual :: MonadSession m => m ('Cap ctx (R (Int :!> Off '[Wk Eps, V]))) ('Cap ctx Eps) Int
+prog_recursion_dual = recurse $ go 0
+    where 
+      go n = do
+        send n
+        (weaken0 >> eps n) <&> (var $ go (n + 1))
